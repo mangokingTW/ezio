47e044e90e07c24283e626e1e3484003536a8e9c
mangokingtw <yench@cs.nctu.edu.tw>
2018-03-25 22:07:19 +0800

    disable verbose output

--- utils/partclone_create_torrent.py
+++ utils/partclone_create_torrent.py
@@ -5,18 +5,18 @@ import sys
 import re
 
 data = sys.stdin.read()
-print data
+# print data
 
 # collect sha1
 piece_hash = re.findall(r'^sha1: (.*)$', data, re.M)
-print piece_hash
+# print piece_hash
 
 # collect offset and length
 offset = re.findall(r'^offset: (.*)$', data, re.M)
 length = re.findall(r'^length: (.*)$', data, re.M)
 
-print "offset: ", offset
-print "length: ", length
+# print "offset: ", offset
+# print "length: ", length
 
 fs = lt.file_storage()
 fs.set_piece_length(16 * 1024 * 1024) # 16MiB
@@ -27,8 +27,8 @@ for o, l in zip(offset, length):
 torrent = lt.create_torrent(fs, 16 * 1024 * 1024, flags=0)
 torrent.set_creator("Test")
 
-print "file:", torrent.files().num_files()
-print torrent.files().file_path(0)
+# print "file:", torrent.files().num_files()
+# print torrent.files().file_path(0)
 
 for index, h in enumerate(piece_hash):
     torrent.set_hash(index, h.decode('hex'))

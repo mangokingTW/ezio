a5cf4bc2ddd89109c79f8197812e766d22a453da
mangokingtw <yench@cs.nctu.edu.tw>
2018-05-23 22:33:46 +0800

    updatet

--- utils/startup.sh
+++ utils/startup.sh
@@ -3,34 +3,9 @@
 # Get tftp server from $siaddr
 TFTP=$(cat /tftp)
 
+busybox tftp -g -l ezio.sh -r ezio/ezio.sh $TFTP
+sh ezio.sh
 
-# download config file
-busybox tftp -g -l ezio.conf -r ezio.conf $TFTP
-
-# parsing config file
-while read line
-do
-    if echo $line | grep -F = &>/dev/null
-    then
-        varname=$(echo "$line" | cut -d '=' -f 1)
-        export $varname=$(echo "$line" | cut -d '=' -f 2-)
-    fi
-done < ezio.conf
-
-# torrent
-TORRENT=$torrent
-# get file
-[ -n "$TORRENT" ] && busybox tftp -g -l $TORRENT -r $TORRENT $TFTP
-
-# magnet uri
-MAGNET=$magnet
-[ -z "$TORRENT" ] && TORRENT=$MAGNET
-
-# target disk
-TARGET=$disk
-
-# exec ezio
-static-ezio "$TORRENT" "$TARGET"
-
-echo "Clone done. Shutdown."
-poweroff
+echo "Clone done."
+sleep 5
+poweroff -f
